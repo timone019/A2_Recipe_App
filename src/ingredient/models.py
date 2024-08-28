e4ef35c79bb8c00345c867762ce3f9a42da7a820
timone019 <143154960+timone019@users.noreply.github.com>
2024-08-28 11:13:24 -0700

    resolved merge conflicts

--- src/ingredient/models.py
+++ src/ingredient/models.py
@@ -4,17 +4,9 @@ from django.db import models
 # Ingredient Model
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-<<<<<<< HEAD
     quantity = models.DecimalField(max_digits=5, decimal_places=2)
     unit = models.CharField(max_length=20, default='unit')
     recipe = models.ForeignKey('recipe.Recipe', on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
         return f"{self.name} ({self.quantity}) ({self.unit})"
-=======
-    quantity = models.CharField(max_length=50)
-    recipe = models.ForeignKey('recipe.Recipe', on_delete=models.CASCADE, related_name='ingredients')
-
-    def __str__(self):
-        return f"{self.name} ({self.quantity})"
->>>>>>> 0dbf1a9c7f88e5af25e02f89b59da443e70065f8
--- src/recipe/views.py
+++ src/recipe/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 def home(request):
     return render(request, 'recipe/home.html')
-=======
->>>>>>> 0dbf1a9c7f88e5af25e02f89b59da443e70065f8
