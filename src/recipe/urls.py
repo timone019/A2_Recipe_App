a20971c838ec91538d823c84f79a31d2f27401a9
timone019 <143154960+timone019@users.noreply.github.com>
2024-08-29 15:21:08 -0700

    2.5 - pre test period

--- src/recipe/urls.py
+++ src/recipe/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, recipe_list, recipe_detail
 
 app_name = 'recipe' 
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('recipes/', recipe_list, name='recipe_list'),
+    path('recipe/<int:recipe_id>/', recipe_detail, name='recipe_detail'),
 ]
--- src/recipe/views.py
+++ src/recipe/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
 # Create your views here.
 def home(request):
+    # recipes = Recipe.objects.all()
     return render(request, 'recipe/home.html')
+
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipe/recipe_list.html', {'recipes': recipes})
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
